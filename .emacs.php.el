46a26145a48a493b68648bfdceb74ee2bedab311
Gunnar Wrobel <p@rdus.de>
2009-02-23 06:57:13 +0000

    Settings for running unit tests in emacs.

---
+++ .emacs.php.el
@@ -0,0 +1,21 @@
+(setq current-dir (file-name-directory load-file-name))
+
+(setq liblocs (concat ":"
+		      current-dir
+		      "lib:"
+                      "/usr/share/php5:"
+                      "/usr/share/php\""))
+
+(setq logopts (concat " -d log_errors=1"
+                      " -d error_log=\""
+		      current-dir
+		      "log/php-errors.log\" "))
+
+(setq phpunitoptions (concat liblocs
+                             logopts))
+
+(setq phpoptions (concat liblocs
+                         logopts
+                         " -c \""
+			 current-dir
+			 "php.ini\" "))
